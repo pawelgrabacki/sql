b86359fd041ab55fbe9886ceb52a922cb725b11e
Paweł Grabacki <paw.grabacki@gmail.com>
2024-12-23 20:48:41 +0100

    init

--- src/basics.sql
+++ src/basics.sql
@@ -10,7 +10,19 @@ DataZatrudnienia date,
 StawkaMiesieczna decimal(10,2)
 )
 
+
 create table JezykiObce(
 JezykObcy varchar(30) primary key
 
+
 )
+
+ create table PracownicyJezykiObce(
+ IdPracownika int,
+ JezykObcy varchar(30) references JezykiObce(JezykObcy) on delete no action on update cascade,
+ constraint kppjo primary key (IdPracownika, JezykObcy),
+ Poziom varchar(30),
+constraint rpjop foreign key(IdPracownika) references Pracownicy(IdPracownika)
+on delete cascade on update no action
+)
+
